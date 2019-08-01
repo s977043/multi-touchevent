2e4f8ecb0abd7a5e81fe9ed12c4c8feab80dd536
小峯 将威 <m_komine@wanaps.jp>
2019-08-01 18:15:50 +0900

    デモ追加

---
+++ demo/stamp.js
@@ -0,0 +1,85 @@
+'use strict';
+
+(function (global, $, Spec) {
+	if (!$) throw new Error('jQuery  が読み込まれていません');
+
+	Array.from = undefined;
+	Array.from = Array.from || function (arraylike) {
+		var a = [];
+		for (var i = 0; i < arraylike.length; i++) {
+			a[i] = arraylike[i];
+		}
+		return a;
+	};
+
+	///=====================================================================================================================
+	/// @brief 　Digishot クラス
+	///
+	/// @param  options             {Object}                (required) 初期化オプション
+	///             .uri            {String}                (required) 認識サーバのURI
+	///             .timeout        {Int}                   (optional) 認識のタイムアウト値[ミリ秒](デフォルト　3000)
+	///             .hint           {String}                (optional) 環境推定ヒント(デフォルト 'standard/ A')
+	///             .pointNum       {Int}                   (optional) 導電点の数(省略した場合は検出端子数のチェックをしない)
+	///             .spec           {Spec}                  (optional) Spec.js のインスタンス
+	///=====================================================================================================================
+	global.Digishot = function Digishot(options) {
+		this._uri = options.uri + '';
+		this._timeout = options.timeout - 0 || 3000;
+		this._hint = options.hint || 'standard/A';
+		this._pointNum = options.pointNum - 0;
+		this._spec = options.spec || new Spec();
+	};
+
+	///=====================================================================================================================
+	/// @brief  サーバにタッチ情報のリクエストを投げる
+	///
+	/// @param  event               {TouchEvent}            (required) タッチイベント
+	/// @param  detectHandler       {function(Block)}       (optional) スタンプ認識イベントハンドラー
+	/// @param  nonDetectHandler    {function(String)}      (optional) 不認識イベントハンドラー
+	/// @param  errorHandler        {function(Int,String)}  (optional) 通信エラーイベントハンドラー
+	///=====================================================================================================================
+	Digishot.prototype.postQuery = function (event, detectHandler, nonDetectHandler, errorHandler) {
+		var touches = (event.originalEvent || event).touches;
+
+		if (this._pointNum && this._pointNum !== touches.length) {
+			return;
+		}
+
+		//	サーバに送信する情報
+		var query = {
+			touches: Array.from(touches).map(function (t) {
+				return { x: t.pageX, y: t.pageY };
+			}),
+			orientation: global.orientation,
+			device: this._spec.DEVICE,
+			displayDpr: this._spec.DISPLAY_DPR,
+			displayInch: this._spec.DISPLAY_INCH,
+			displayLong: this._spec.DISPLAY_LONG,
+			displayShort: this._spec.DISPLAY_SHORT,
+			estimationHint: this._hint + ''
+		};
+
+		console.log(query);
+
+		//	サーバに送信
+		$.ajax(this._uri, {
+			type: 'POST',
+			contentType: 'application/json; charset=utf-8',
+			data: JSON.stringify(query),
+			dataType: 'json',
+			timeout: this._timeout
+		}).then(function (json) {
+			//	通信に成功
+			if (json.block) {
+				//	認識に成功
+				detectHandler && detectHandler(json.block);
+			} else {
+				//	認識に失敗
+				nonDetectHandler && nonDetectHandler(json.error);
+			}
+		}, function (jqxhr, status, error) {
+			//	通信に失敗
+			errorHandler && errorHandler(status, error);
+		});
+	};
+})(window, jQuery, Spec);
---
+++ demo/ticket.js
@@ -0,0 +1,63 @@
+'use strict';
+
+window.addEventListener('load', function () {
+	// 読み込み中のアニメーションを終了
+	document.querySelector('.loading').style.display = 'none';
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+	// タッチ開始時の処理
+	document.addEventListener('touchstart', function onTouchStart(event) {
+		if (event.touches.length !== 4) return false;
+
+		// ブロックの認識を開始する
+		// エラーメッセージを一旦非表示に
+		var stamp = document.querySelector('.stamp_p');
+		var error = document.querySelector('.error-message');
+
+		// 画面を暗くする
+		var blackout = document.querySelector('.overlay-black');
+
+		blackout.style.display = 'block';
+
+		error.style.display = 'none';
+
+		new Digishot({ uri: '../ticketServlet' }).postQuery(event, function (block) {
+			// ブロックの認識が成功した
+			// 白く明滅
+			var whiteout = document.querySelector('.overlay-white');
+
+			blackout.style.display = 'none';
+			whiteout.style.display = 'block';
+
+			// ブロックを表示する
+
+			stamp.style.display = 'block';
+			stamp.style.left = block.centerX + 'px';
+			stamp.style.top = block.centerY + 'px';
+			stamp.style.transform = stamp.style.webkitTransform = 'translate(-50%, -50%) rotate(' + block.rotation + 'rad)';
+
+			// タッチイベントを取り除く
+			document.removeEventListener('touchstart', onTouchStart);
+		}, function () {
+			// エラーを表示
+			blackout.style.display = 'none';
+
+			if (stamp.style.display !== 'block') {
+				error.style.display = 'block';
+			}
+		}, function () {
+			// エラーを表示
+			blackout.style.display = 'none';
+
+			if (stamp.style.display !== 'block') {
+				error.style.display = 'block';
+			}
+		});
+	}, false);
+
+	// デフォルトのタッチイベントを無効化する
+	document.addEventListener('touchstart', function (e) {
+		return e.preventDefault();
+	}, false);
+}, false);
